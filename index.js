58c6d09ae22e74b945ec40c23a92e28a885daf0f
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-03 18:45:19 -0600

    reading fileswith async & await and found password

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mostFrequentWord = (text) => {
   const words = text.toLowerCase().match(/[^_\W]+/g);
   const tally = {};
   let mostFrequentWord = null;
-  
+
   words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+    tally[word] = (tally[word] || 0) + 1;
+    if (!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
       mostFrequentWord = word;
   });
   return mostFrequentWord;
@@ -15,6 +15,27 @@ const mostFrequentWord = (text) => {
 
 const findPassword = async () => {
   // Your code goes here
+  try {
+    const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
+    const poem2FileName = `poems/${mostFrequentWord(poem1)}.txt`;
+    console.log(poem2FileName);
+  } catch (error) {
+    throw error;
+  };
+  try {
+    const poem2 = await readFile('poems/happy.txt', "utf-8");
+    const poem3FileName = `poems/${mostFrequentWord(poem2)}.txt`;
+    console.log(poem3FileName);
+  } catch (error) {
+    throw error;
+  };
+  try {
+    const poem3 = await readFile('poems/you.txt', "utf-8");
+    const poem4FileName = `poems/${mostFrequentWord(poem3)}.txt`;
+    console.log(poem4FileName);
+  } catch (error) {
+    throw error;
+  };
 }
 
 findPassword();
